a2cb8fb0f48beb048a0a9e21ba50cc5d6d21c792
DendroIDE-Code <dendroide.code@gmail.com>
2025-02-27 23:58:29 -0500

    chore(project): setup initial project for Firebase Android Kotlin course

---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "learning-firebase-android-kotlin-aristidevs"
+include(":app")
